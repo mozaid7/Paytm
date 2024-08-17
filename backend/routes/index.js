877a7b051d552186529980fb0701fd759c5dc64c
Mohammed Zaid <mohammedzee98@gmail.com>
2024-08-17 11:40:44 +0530

    routers aggregated

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
+const accountRouter = require("./account");
 const userRouter = require("./user");
+
 router.use("/user", userRouter);
+router.use("/account", accountRouter);
 
 module.exports = router;
