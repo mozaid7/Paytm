40652aa00db0e6ea00ce70c808d3479ebf89c876
Mohammed Zaid <mohammedzee98@gmail.com>
2024-08-17 20:43:40 +0530

    completed frontend and combined with backend & created a new component SendMoney

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,13 +1,42 @@
-import { Appbar } from "../components/Appbar"
-import { Balance } from "../components/Balance"
-import { Users } from "../components/Users"
+import React, { useEffect, useState } from "react";
+import Appbar from "../components/Appbar";
+import Users from "../components/Users";
+import Balance from "../components/Balance";
+import axios from "axios";
 
-export const Dashboard = () => {
-    return <div>
-        <Appbar />
-        <div className="m-8">
-            <Balance value={"10,000"} />
-            <Users />
-        </div>
+const Dashboard = () => {
+  const [balance, setBalance] = useState("");
+
+  useEffect(() => {
+    const fetchBalance = async () => {
+      try {
+        const response = await axios.get(
+          "http://localhost:3001/api/v1/account/balance",
+          {
+            headers: {
+              Authorization: `Bearer ${localStorage.getItem("token")}`,
+            },
+          }
+        );
+        setBalance(response.data.balance);
+      } catch (error) {
+        console.error("Error fetching balance:", error);
+        // Handle the error appropriately
+      }
+    };
+
+    fetchBalance();
+  }, []);
+
+  return (
+    <div>
+      <Appbar />
+      <div className="m-8">
+        <Balance value={balance} />
+        <Users />
+      </div>
     </div>
-}
+  );
+};
+
+export default Dashboard;
